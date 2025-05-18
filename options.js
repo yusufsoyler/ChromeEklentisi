8d0c18e5338f477a775151ea775c1f0a425e9753
YUSUF <96496312+yusufsoyler@users.noreply.github.com>
2025-05-18 16:17:13 +0300

    Delete options.js

--- options.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Mevcut kısayol tuşunu al
-  chrome.commands.getAll(function(commands) {
-    const clearCookiesCommand = commands.find(command => command.name === "clear-cookies");
-    if (clearCookiesCommand) {
-      document.getElementById('currentShortcut').textContent = clearCookiesCommand.shortcut || 'Atanmamış';
-    }
-  });
-}); 
